60dfd911f1e038811ea6b0c03b2712d24cfba7f9
htamayo93 <htamayo@student.rcc.edu>
2016-03-09 12:39:56 -0800

    Signed-off-by: htamayo93 <htamayo@student.rcc.edu>

---
+++ Homework/Gaddis_8thEd_Chap9_Probl2/main.cpp
@@ -0,0 +1,36 @@
+/* 
+ * File:   main.cpp
+ * Author: Heidy Tamayo
+ * Created on February 29, 2016, 2:27 PM
+ * Purpose: Sorts the names and looks for the name the user has inputted.
+ */
+
+//System Libraries
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+//User Libraries
+
+//Global Constants
+
+//Functional Prototypes
+void sortArr();
+void aveArr();
+
+//Execution Begins Here
+int main(int argc, char** argv) {
+
+    float scores;
+    float avescor;
+    int testSco;
+    
+    
+    cout<<"Please enter the number of how many scores you would like to have saved."<<endl;
+    cin>>testSco;
+    for(count=1;count<=testSco;count++)
+    {
+        cout<<"Scor"
+    }
+    return 0;
+}
