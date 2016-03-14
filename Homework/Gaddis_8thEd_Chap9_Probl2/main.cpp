02d44e7cea417a81e1d863833ce49c45c43c4def
htamayo93 <htamayo@student.rcc.edu>
2016-03-14 15:49:22 -0700

    Signed-off-by: htamayo93 <htamayo@student.rcc.edu>

--- Homework/Gaddis_8thEd_Chap9_Probl2/main.cpp
+++ Homework/Gaddis_8thEd_Chap9_Probl2/main.cpp
@@ -15,22 +15,49 @@ using namespace std;
 //Global Constants
 
 //Functional Prototypes
-void sortArr();
-void aveArr();
+void sortArr(float *,int);
+void aveArr(float *,int);
 
 //Execution Begins Here
 int main(int argc, char** argv) {
 
-    float scores;
+    float *scores;
     float avescor;
     int testSco;
-    
-    
+
     cout<<"Please enter the number of how many scores you would like to have saved."<<endl;
     cin>>testSco;
-    for(count=1;count<=testSco;count++)
+    scores=new float [testSco];
+    cout<<"Enter your test scores"<<endl;
+    for(int count=0;count<testSco;count++)
     {
-        cout<<"Scor"
+        cout<<"Score "<<count+1<<endl;
+        cin>>*(scores+count);
     }
+    sortArr(scores,testSco);
+    aveArr(scores,testSco);
     return 0;
+}
+void sortArr(float *scores, int testSco)
+{
+    for(int pos=0;pos<testSco-1;pos++)
+    {
+        float minVal=[pos];
+        for(int i=pos+1;i<testSco;i++)
+        {
+            if([i]<[pos])
+            {
+                minVal=[i];
+            }
+        }
+
+    }
+}
+void aveArr(float *scores, int testSco)
+{
+    for(int count=0;count<testSco;count++)
+    {
+        cout<<[count]<<"  ";
+        cout<<endl;
+    }
 }
