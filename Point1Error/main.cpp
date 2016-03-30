ee3fbb75e261390de9adcaf1dc64c54142db06cf
htamayo93 <htamayo@student.rcc.edu>
2016-03-30 14:05:30 -0700

    Signed-off-by: htamayo93 <htamayo@student.rcc.edu>

---
+++ Point1Error/main.cpp
@@ -0,0 +1,26 @@
+/* 
+ * File:   main.cpp
+ * Author: Heidy Tamayo
+ * Created on March 30, 2016, 1:42 PM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+//User Libraries
+
+//Function Prototypes
+
+//Execution Begins Here
+int main(int argc, char** argv) {
+    //Declare variables
+    float francshn=0.2f;      //Fraction to sum
+    int nLoops    =10000000;  //10 Million sum of fraction
+    float sum     =0.0f;      //Initialize the sum to zero
+    float answer;             //Exact answer
+    
+    //Loop  
+    return 0;
+}
+
