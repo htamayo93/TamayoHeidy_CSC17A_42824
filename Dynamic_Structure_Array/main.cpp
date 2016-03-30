f804e0d73f6d1c16b7914b2b12405024848af962
htamayo93 <htamayo@student.rcc.edu>
2016-03-30 13:24:14 -0700

    Signed-off-by: htamayo93 <htamayo@student.rcc.edu>

--- Dynamic_Structure_Array/main.cpp
+++ Dynamic_Structure_Array/main.cpp
@@ -1,8 +1,8 @@
 /* 
  * File:   main.cpp
- * Author: rcc
- *
+ * Author: Heidy Tamayo
  * Created on March 28, 2016, 1:50 PM
+ * Purpose: Learning Structures 
  */
 
 //System Libraries
@@ -41,17 +41,34 @@ int main(int argc, char** argv) {
     //Exit stage right
     return 0;
 }
-void prntAry(Array *a, int perLine)
+//000000011111111112222222222333333333344444444445555555555666666666677777777778
+//345678901234567890123456789012345678901234567890123456789012345678901234567890
+//                               Print Array
+//Inputs
+//     a->Array structure
+//     perLine->Number of integers to display per row
+////////////////////////////////////////////////////////////////////////////////
+void prntAry(Array *a,int perLine)
 {
     //Output the Array 
     cout<<endl;
-    for (int i=0;i<n;i++)
+    for (int i=0;i<a->size;i++)
     {
-        
+        cout<<a->data[i]<<" ";
+        if(i%perLine==(perLine-1))cout<<endl;
     }
+    cout<<endl;
 }
+//000000011111111112222222222333333333344444444445555555555666666666677777777778
+//345678901234567890123456789012345678901234567890123456789012345678901234567890
+//                                Fill Array
+//Inputs
+//     n->Size of the array
+//Outputs
+//     a->Array structure filled with 2 digit integers
 ////////////////////////////////////////////////////////////////////////////////
-Array *fillAry(int n){
+Array *fillAry(int n)
+{
     //Declare and allocate the structure
     Array *a=new Array;
     
@@ -60,7 +77,7 @@ Array *fillAry(int n){
     a->data=new int [n];
     
     //Loop and fill with 2 digits numbers
-    for (int i=0,i<n;i++)
+    for (int i=0;i<n;i++)
     {
         a->data[i]=rand()%90+10;
     }
