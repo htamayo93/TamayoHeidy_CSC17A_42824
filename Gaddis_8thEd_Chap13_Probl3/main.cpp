ce02bb25c0858ec3252e43aa385f239c48a65c35
htamayo93 <htamayo@student.rcc.edu>
2016-04-27 14:02:03 -0700

    Signed-off-by: htamayo93 <htamayo@student.rcc.edu>

--- Gaddis_8thEd_Chap13_Probl3/main.cpp
+++ Gaddis_8thEd_Chap13_Probl3/main.cpp
@@ -4,12 +4,32 @@
  * Created on April 27, 2016, 12:08 PM
  */
 
+//System Libraries
 #include <iostream>
 #include <string>
+//User Libraries
+#include "Car.h"
 using namespace std;
 
+//Global Constants
+
+//Function Prototypes
+
+//Execution begins Here
 int main(int argc, char** argv) {
-    string hello;
+    //Declare and initialize the variables
+    int year;
+    string make;
+    Car info;
+    
+    cout<<"Please enter the model year of the car."<<endl;
+    cin>>year;
+    info.setmodel(year);
+    cout<<"Please enter the make of the car."<<endl;
+    cin>>make;
+    info.setmake(make);
+    
+    //Exit stage right
     return 0;
 }
 
